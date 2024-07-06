b64369015e830e5aad6d90b3d8bd583877590ee9
Abdelrahman Samir <60700731+abdelrahman04@users.noreply.github.com>
2024-07-06 23:00:10 +0300

    Update client.py
    compressing ports

--- client.py
+++ client.py
@@ -1,24 +1,28 @@
 import socket
-
-# Define the ports to connect to
-ports = [9000, 9001, 9002, 9003, 9004, 9005]
-
-# Data to send to each port
-data_to_send = [f"{i},{i}" for i in range(len(ports))]
-
+import sys
 
 # Function to send data to a specific port
 def send_data(port, data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         # Connect to the server
         client_socket.connect(('192.168.22.235', port))
-
         # Send the data
         client_socket.sendall(data.encode('utf-8'))
-
         print(f"Sent data to port {port}: {data}")
 
+        while True:
+            received_data = client_socket.recv(1024).decode('utf-8')
+            if received_data:
+                print(f"{received_data}")
+                break
 
-# Send data to each port
-for port, data in zip(ports, data_to_send):
-    send_data(port, data)
+def main():
+    port = 9000
+    the_args = [sys.argv[1], sys.argv[2], sys.argv[3]]
+    data_to_send = '.'.join(the_args)
+
+    print(data_to_send)
+    send_data(port, data_to_send)
+
+if __name__ == "__main__":
+    main()
